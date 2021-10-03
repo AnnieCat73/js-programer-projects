a0f8af209236d0a527987a22d03231967750f185
annie_i <anniecats673@gmail.com>
2021-10-03 09:04:27 +0100

    Accordion part 2

--- accordion/01.starter/accordion-01.starter/js/main.js
+++ accordion/01.starter/accordion-01.starter/js/main.js
@@ -24,7 +24,7 @@ When we click an accordion’s header, we want to open the accordion.
 Since we want to click the accordion’s header, we should wrap the
 header’s content in a <button> element.
 
-<header class="accordion__header">
+<header class="accordion-header">
   <h2>
     <button>
       <span class="accordion-title">Cheese</span>
@@ -32,4 +32,65 @@ header’s content in a <button> element.
     </button>
   </h2>
 </header>
+////////////
+To open the accordion, we can add an is-open class to the
+.accordion
+
+<!-- Opened accordion -->
+<div class="accordion is-open"> ...</div>
+/* Opens the accordion
+.accordion.is-open .accordion-content {
+  display: grid;
+}
+
+///////////////////
+
+//Indicators
+
+f you press +, you will open the accordion
+If you press -, you will close the accordion
+
+.accordion .indicator-plus {
+  display: block;
+}
+
+.accordion .indicator-minus {
+  display: none;
+}
+
+When the accordion is opened, we need to show the - icon.
+We also need to hide the + icon.
+
+.accordion.is-open  .indicator-minus {
+  display: block;
+}
+
+.accordion.is-open  .indicator-plus {
+  display: none;
+}
+
+//Opening the first accordion with Javascript
 */
+
+const firstAccordion = document.querySelector(".accordion");
+//console.log(firstAccordion);
+const firstAccordionHeader = document.querySelector(".accordion__header");
+//console.log(firstAccordionHeader)//
+
+/*firstAccordionHeader.addEventListener('click', event => {
+  // Do something
+}) To open the first accordion’s content, you add the is-open class to the 
+first accordion. SO ADD THE CLASS TO CSS so becomes this*/
+
+firstAccordionHeader.addEventListener('click', event => {
+  if (firstAccordion.classList.contains('is-open')) {
+    firstAccordion.classList.remove('is-open')
+  } else {
+    firstAccordion.classList.add('is-open')
+  }
+})
+
+//or
+/*firstAccordionHeader.addEventListener('click', event => {
+  firstAccordion.classList.toggle('is-open')
+})*/
