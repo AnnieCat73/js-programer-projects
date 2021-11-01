6f2e2f8d6b71f8d6bfd10f48e3e67b2fb7bda28c
annie_i <anniecats673@gmail.com>
2021-11-01 05:38:32 +0000

    Transitions added for off-canvas-menu

--- off-canvas-menu-js/02.basic/off-canvas-menu-02.basic/js/main.js
+++ off-canvas-menu-js/02.basic/off-canvas-menu-02.basic/js/main.js
@@ -5,3 +5,22 @@ const body = document.body
 button.addEventListener('click', _ => {
   body.classList.toggle('offsite-is-open')
 })
+
+//WITH ANIMATIONS
+
+/*In CSS
+
+ANIMATING  BUTTON
+button {
+  border: 2px solid currentColor;
+  transition: background-color 0.3s ease-out;
+}
+
+TRANSITIONS WHEN CONTAINERS SLIDE IN AND OUT
+.site-container {
+  transition: transform 0.3s ease-out;
+}
+
+.offsite-container {
+  transition: transform 0.3s ease-out;
+}*/
